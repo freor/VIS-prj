729774349f1bb028df5aca2c9bc87a7f5870d845
befreor <befreor@gmail.com>
2019-11-20 14:28:38 +0900

    image top-100

--- get_image.py
+++ get_image.py
@@ -2,12 +2,19 @@ import json
 import os
 import time
 
-jsonfile = open('./youtube-json/CAvideos.json', 'r')
+jsonfile = open('./youtube-json/new_CAvideos.json', 'r')
 
-d = json.load(jsonfile)
+d = json.load(jsonfile)["nodes"]
 
+for i in d:
+    img_id = i["id"]
+    link = i["group"]["thumbnail_link"]
+    os.system("wget -O thumbnail_img/" + img_id + ".jpg " + link)
+
+'''
 for i in d:
     print(i)
     os.system("wget -O " + d[i]["thumbnail_link"])
     time.sleep(5)
+'''
 
